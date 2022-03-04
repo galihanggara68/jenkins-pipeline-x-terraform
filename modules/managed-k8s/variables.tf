1bd6b2df8f9bd18d924e39ebd1e2787b7022d031
galihanggara68 <galihanggara68@gmail.com>
2022-03-04 15:49:01 +0700

    * Comment default value on variable

--- modules/managed-k8s/variables.tf
+++ modules/managed-k8s/variables.tf
@@ -1,38 +1,38 @@
 variable "k8s_name_prefix" {
   description = "The name prefix used to create managed kubernetes cluster."
-  default     = "tf-ack"
+  # default     = "tf-ack"
 }
 
 variable "cpu_core_count" {
-  default = 2
+#   default = 2
 }
 
 variable "memory_size" {
-  default = 4
+  # default = 4
 }
 
 variable "worker_number" {
-  default = 2
+  # default = 2
 }
 
 variable "worker_data_disk_size" {
-  default = 100
+  # default = 100
 }
 
 variable "password" {
-  default = "P@assword123"
+  # default = "P@assword123"
 }
 
 variable "vpc_cidr_block" {
-  default = "10.1.0.0/21"
+  # default = "10.1.0.0/21"
 }
 
 variable "vswitch_cidr_block" {
-  default = "10.1.1.0/24"
+  # default = "10.1.1.0/24"
 }
 
 variable "resource_group_id" {
-  default = "rg-aek2kdtpdwb7qfi"
+  # default = "rg-aek2kdtpdwb7qfi"
 }
 
 variable "pod_cidr" {
