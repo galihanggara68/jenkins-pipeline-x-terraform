d3d2eef29033b71473188cf2cf5a3301aa60cf70
galihanggara68 <galihanggara68@gmail.com>
2022-03-04 17:27:08 +0700

    * Comment default value

--- variables.tf
+++ variables.tf
@@ -1,44 +1,44 @@
 variable "k8s_name_prefix" {
   description = "The name prefix used to create managed kubernetes cluster."
-  default     = "tf-ack"
+  # default     = "tf-ack"
 }
 
 variable "cpu_core_count" {
-  default = 2
+  # default = 2
 }
 
 variable "memory_size" {
-  default = 4
+  # default = 4
 }
 
 variable "worker_number" {
-  default = 2
+  # default = 2
 }
 
 variable "worker_data_disk_size" {
-  default = 100
+  # default = 100
 }
 
 variable "password" {
-  default = "P@assword123"
+  # default = "P@assword123"
 }
 
 variable "vpc_cidr_block" {
-  default = "10.1.0.0/21"
+  # default = "10.1.0.0/21"
 }
 
 variable "vswitch_cidr_block" {
-  default = "10.1.1.0/24"
+  # default = "10.1.1.0/24"
 }
 
 variable "pod_cidr" {
-  default = "172.20.0.0/16"
+  # default = "172.20.0.0/16"
 }
 
 variable "service_cidr" {
-  default = "172.21.0.0/20"
+  # default = "172.21.0.0/20"
 }
 
 variable "resource_group_id" {
-  default = "rg-aek2kdtpdwb7qfi"
+  # default = "rg-aek2kdtpdwb7qfi"
 }
